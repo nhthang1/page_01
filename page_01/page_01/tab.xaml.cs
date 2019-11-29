e51ec2338b13d9fd004098996690264bdfca6992
nhthang1 <50260034+nhthang1@users.noreply.github.com>
2019-11-29 20:41:17 +0700

    first commit

---
+++ page_01/page_01/tab.xaml.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace page_01
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class tab : Shell
+    {
+        public tab()
+        {
+            InitializeComponent();
+            
+        }
+        
+    }
+}
